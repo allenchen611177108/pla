350de84f841e0c761cb9495d81f0e60d454f0cbb
Arlun <410777028@mail.nknu.edu.tw>
2023-01-02 20:10:06 +0800

    [fix] change unit test context

--- test_module.py
+++ test_module.py
@@ -3,11 +3,6 @@ from func import Module
 a = [7, -4, 5, 8, -10, 4, 5, 0]
 b = [1, -1, 1, 1, -1, 1, 1, -1]
 
-def test_sign_positive():
-    assert Module.activate_sign(7) is 1
-
-def test_sign_negative():
-    assert Module.activate_sign(-1) is -1
-
-def test_sign_zero():
-    assert Module.activate_sign(0) is -1
+def test():
+    for i in range(len(a)):
+        assert Module.activate_sign(a[i]) is b[i]
